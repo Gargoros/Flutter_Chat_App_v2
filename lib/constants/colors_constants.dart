ca2e9b113781bd4f90fd33b83ef31cbf317c3954
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-01 02:23:51 +0300

    feat: init file with colors constants

---
+++ lib/constants/colors_constants.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFF131C21);
+const textColor = Color(0xFFF1F1F2);
+const appBarColor = Color(0xFF1F2C34);
+const webAppBarColor = Color(0xFF2A2F32);
+const messageColor = Color(0xFF056062);
+const senderMessageColor = Color(0xFF252D31);
+const tabColor = Color(0xFF00A783);
+const searchBarColor = Color(0xFF323739);
+const dividerColor = Color(0xFF252D32);
+const chatBarMessage = Color(0xFF1E2428);
+const mobileChatBoxColor = Color(0xFF1F2C34);
+
+const appBarIconAndTextColor = Color(0xFF9E9E9E);
