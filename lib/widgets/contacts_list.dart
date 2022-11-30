c6aa4cc07d7dfabeeb027dff343f0ac08e7f8fe7
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-01 02:21:54 +0300

    feat: init contacts widget, for build contacts ui on a screen

---
+++ lib/widgets/contacts_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/constants/colors_constants.dart';
+
+import '../constants/text_style_constants.dart';
+import '../data/dummy_data/info.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: ListView.separated(
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 6.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        NetworkImage(info[index]["profilePic"].toString()),
+                    radius: 30,
+                  ),
+                  title: Text(
+                    info[index]["name"].toString(),
+                    style: contactTitle,
+                  ),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 6),
+                    child: Text(
+                      info[index]["message"].toString(),
+                      style: contactSubTitle,
+                    ),
+                  ),
+                  trailing: Text(
+                    info[index]["time"].toString(),
+                    style: contactTrailing,
+                  ),
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(
+                color: dividerColor,
+              ),
+          itemCount: info.length),
+    );
+  }
+}
