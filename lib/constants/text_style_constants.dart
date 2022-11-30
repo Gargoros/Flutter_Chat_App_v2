f3a12489c3fbd35f9dfd56bbe21160ba027dd4a7
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-01 02:24:42 +0300

    feat: init file with textStyle constants

---
+++ lib/constants/text_style_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const appTitleStyle = TextStyle(
+    fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xFF9E9E9E));
+
+const contactTitle = TextStyle(fontSize: 18);
+const contactSubTitle = TextStyle(fontSize: 15);
+const contactTrailing = TextStyle(fontSize: 13, color: Color(0xFF9E9E9E));
