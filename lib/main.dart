96eb556e6f72802e00e1ed998a52a78ed55c764c
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-02 00:52:27 +0300

    refactor: rename wed screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
+        webScreenLayout: WebLayoutScreen(),
       ),
     );
   }
