b0417b73b85324febb5bb3f9f06a1e0eb0a4a5f2
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-01 13:56:52 +0300

    feat: init new style for web screen

--- lib/constants/text_style_constants.dart
+++ lib/constants/text_style_constants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/constants/colors_constants.dart';
+import 'text_constants.dart';
 
 const appTitleStyle = TextStyle(
     fontWeight: FontWeight.bold, fontSize: 20, color: Color(0xFF9E9E9E));
@@ -11,3 +13,34 @@ const webBackgroundImage = BoxDecoration(
     image: DecorationImage(
         fit: BoxFit.cover,
         image: AssetImage("assets/images/backgroundImage.png")));
+
+const webProfileBarDecoration = BoxDecoration(
+    color: webAppBarColor,
+    border: Border(right: BorderSide(color: dividerColor)));
+
+const webSearchBarDecoration = BoxDecoration(
+  border: Border(
+    bottom: BorderSide(color: dividerColor),
+  ),
+);
+
+const hintTextStyle = TextStyle(fontSize: 14);
+
+final webSearchBarStyle = InputDecoration(
+    hintText: searchHintTextField,
+    hintStyle: hintTextStyle,
+    border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+        borderSide: const BorderSide(
+          width: 0,
+          style: BorderStyle.none,
+        )),
+    filled: true,
+    fillColor: searchBarColor,
+    prefixIcon: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Icon(
+          Icons.search,
+          size: 20,
+        )),
+    contentPadding: const EdgeInsets.all(10));
