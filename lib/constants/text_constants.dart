03c1debeb6231812be501b5a39bdedecb5fc73a3
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-01 13:56:12 +0300

    feat: init new const for searchBar

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -1,7 +1,7 @@
-import 'dart:ui';
-
 const appTitle = "Let's Talk";
 
 const chatTabText = "Chats";
 const statusTabText = "Status";
 const callsTabText = "Calls";
+
+const searchHintTextField = "Search or start new chat";
