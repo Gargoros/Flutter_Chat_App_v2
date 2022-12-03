4f710115017fe7c25840ed95e97cd632de89d9bc
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-03 20:33:04 +0300

    feat: init some text and some style const for landing screen

---
+++ lib/features/landing/constants/landing_const.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/constants/colors_constants.dart';
+
+const landingScreenTitleText = "Welcom To Let's Talk";
+const lanndingScreenSubText =
+    'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.';
+const landingButtonText = "AGREE AND CONTINUE";
+
+const landingScreenTitleTextStyle =
+    TextStyle(fontSize: 33, fontWeight: FontWeight.w600);
+const lanndingScreenSubTextStyle = TextStyle(color: appBarIconAndTextColor);
+
+const landingImageLogo = "assets/images/bg.png";
