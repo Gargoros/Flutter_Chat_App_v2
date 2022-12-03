c56deeb463e8ce71d3f70055ab102de4c2100c01
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-04 01:31:27 +0300

    feat: init controller foe auth check

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/features/auth/repository/auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authControllerProvider = Provider(((ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AuthController(authRepository: authRepository);
+}));
+
+class AuthController {
+  final AuthRepository authRepository;
+
+  AuthController({required this.authRepository});
+
+  void singInWithPhone(BuildContext context, String phoneNumber) {
+    authRepository.signInWithPhone(context, phoneNumber);
+  }
+}
