a1fc805b5dc3b88c811d2140b9bf4b625f3f5043
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-03 20:31:48 +0300

    feat: init new login text const

---
+++ lib/features/auth/constants/login_screen_const.dart
@@ -0,0 +1,7 @@
+const loginScreenTitle = "Enter your phone number";
+const loginScreenSubTitleText =
+    "Let's Talk will need to verify your phone number";
+
+const loginTextButtonText = "Pick Country";
+const loginHintTextButton = "phone number";
+const loginCustomButtonText = "NEXT";
