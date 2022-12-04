6c7d46c7be9538bf0f70889cd67dbb9d6acf8806
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-04 18:49:32 +0300

    feat: init new file with otpScreen text and style const

---
+++ lib/features/auth/constants/otp_screen_const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const otpScreenTitleText = "Verifying your number";
+const otpScreenSubText = "We have sent an SMS with a code on your number.";
+const otpScreenTextFieldHintText = " - - - - - - ";
+
+const otpScreenTextFieldTextStyle = TextStyle(fontSize: 30);
