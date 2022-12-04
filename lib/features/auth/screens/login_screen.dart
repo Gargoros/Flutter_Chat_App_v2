630ac0ac4d4dc3d8ca4403e8f221a3cf53722313
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-05 00:16:25 +0300

    refactor: wrap login screen class to consumer extend for send to service user phone number

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -2,18 +2,21 @@ import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/common/widgets/custom_button.dart';
 import 'package:flutter_chat_app/constants/colors_constants.dart';
+import 'package:flutter_chat_app/features/auth/controller/auth_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../common/utils/utils.dart';
 import '../constants/login_screen_const.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends ConsumerStatefulWidget {
   static const routeName = "/login-screen";
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  ConsumerState<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends ConsumerState<LoginScreen> {
   final phoneController = TextEditingController();
   Country? country;
 
@@ -33,6 +36,17 @@ class _LoginScreenState extends State<LoginScreen> {
         });
   }
 
+  void sendPhoneNumber() {
+    String phoneNumber = phoneController.text.trim();
+    if (country != null && phoneNumber.isNotEmpty) {
+      ref
+          .read(authControllerProvider)
+          .singInWithPhone(context, "+${country!.phoneCode}$phoneNumber");
+    } else {
+      showSnackBar(context: context, content: loginSnackBarText);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -42,49 +56,52 @@ class _LoginScreenState extends State<LoginScreen> {
         elevation: 0,
         backgroundColor: backgroundColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Text(loginScreenSubTitleText),
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              TextButton(
-                onPressed: pickCountry,
-                child: const Text(loginTextButtonText),
-              ),
-              SizedBox(
-                height: size.height * 0.005,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  if (country != null) Text("+${country!.phoneCode}"),
-                  SizedBox(
-                    width: size.width * 0.7,
-                    child: TextField(
-                      controller: phoneController,
-                      decoration: const InputDecoration(
-                        hintText: loginHintTextButton,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Text(loginScreenSubTitleText),
+                SizedBox(
+                  height: size.height * 0.04,
+                ),
+                TextButton(
+                  onPressed: pickCountry,
+                  child: const Text(loginTextButtonText),
+                ),
+                SizedBox(
+                  height: size.height * 0.005,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    if (country != null) Text("+${country!.phoneCode}"),
+                    SizedBox(
+                      width: size.width * 0.6,
+                      child: TextField(
+                        keyboardType: TextInputType.phone,
+                        controller: phoneController,
+                        decoration: const InputDecoration(
+                          hintText: loginHintTextButton,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: size.height * 0.2,
-              ),
-              SizedBox(
-                width: size.width * 0.2,
-                child: CustomButton(
-                  buttonText: loginCustomButtonText,
-                  onPressed: () {},
+                  ],
                 ),
-              )
-            ]),
+                SizedBox(
+                  height: size.height * 0.2,
+                ),
+                SizedBox(
+                  width: size.width * 0.2,
+                  child: CustomButton(
+                    buttonText: loginCustomButtonText,
+                    onPressed: sendPhoneNumber,
+                  ),
+                )
+              ]),
+        ),
       ),
     );
   }
