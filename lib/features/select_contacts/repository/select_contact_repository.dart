922e30666bf458ffc43eb8a9827f59fcc979d71b
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-05 01:31:15 +0300

    feat: init provider to this class

--- lib/features/select_contacts/repository/select_contact_repository.dart
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -1,6 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final selectContactRepositoryProvider =
+    Provider(((ref) => SelectContactRepository(
+          firebaseFirestore: FirebaseFirestore.instance,
+        )));
 
 class SelectContactRepository {
   final FirebaseFirestore firebaseFirestore;
