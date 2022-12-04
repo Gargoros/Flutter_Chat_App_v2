21caaba23fe6fb52bfbfc092a8915c285d5426c0
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-05 00:14:52 +0300

    feat: init class that will verify and save  user data

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,9 +1,16 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/common/repositories/common_firebase_storage_repository.dart';
+import 'package:flutter_chat_app/features/auth/screens/user_information_screen.dart';
+import 'package:flutter_chat_app/models/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../common/utils/utils.dart';
+import '../../../screens/mobile_screen_layout.dart';
+import '../constants/user_information_const.dart';
 import '../screens/otp_screen.dart';
 
 final authRepositoryProvider = Provider(((ref) => AuthRepository(
@@ -20,8 +27,8 @@ class AuthRepository {
     try {
       await auth.verifyPhoneNumber(
         phoneNumber: phoneNumber,
-        verificationCompleted: (PhoneAuthCredential credential) async {
-          await auth.signInWithCredential(credential);
+        verificationCompleted: (PhoneAuthCredential phoneAuthCredential) async {
+          await auth.signInWithCredential(phoneAuthCredential);
         },
         verificationFailed: (e) {
           throw Exception(e.message);
@@ -37,6 +44,62 @@ class AuthRepository {
       );
     } on FirebaseAuthException catch (e) {
       showSnackBar(context: context, content: e.message!);
+    }
+  }
+
+  void verifyOTP({
+    required BuildContext context,
+    required String verificationId,
+    required String userOTP,
+  }) async {
+    try {
+      PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: userOTP);
+      await auth.signInWithCredential(phoneAuthCredential);
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        UserInformationScreen.routeName,
+        (route) => false,
+      );
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context: context, content: e.message!);
+    }
+  }
+
+  void saveUserDataToFirebase({
+    required String name,
+    required File? profilePic,
+    required ProviderRef ref,
+    required BuildContext context,
+  }) async {
+    try {
+      String uid = auth.currentUser!.uid;
+      String photoUrl = circularAvatarBackgroundImage;
+      if (profilePic != null) {
+        photoUrl = await ref
+            .read(commonFirebaseStorageRepositoryProvider)
+            .storeFileFirebase(
+              "profilePic/$uid",
+              profilePic,
+            );
+      }
+      var user = UserModel(
+          name: name,
+          uid: uid,
+          profilePic: photoUrl,
+          phoneNumber: auth.currentUser!.uid,
+          groupId: [],
+          isOnline: true);
+
+      await firebaseFirestore.collection("users").doc(uid).set(user.toMap());
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const MobileScreenLayout(),
+          ),
+          (route) => false);
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
     }
   }
 }
