7a166aa54b5703892e2d50ee180344bc62b245d9
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-04 18:50:14 +0300

    feat: initnew function for verify user

--- lib/features/auth/controller/auth_controller.dart
+++ lib/features/auth/controller/auth_controller.dart
@@ -15,4 +15,9 @@ class AuthController {
   void singInWithPhone(BuildContext context, String phoneNumber) {
     authRepository.signInWithPhone(context, phoneNumber);
   }
+
+  void verifyOTP(BuildContext context, String verificationId, String userOTP) {
+    authRepository.verifyOTP(
+        context: context, verificationId: verificationId, userOTP: userOTP);
+  }
 }
