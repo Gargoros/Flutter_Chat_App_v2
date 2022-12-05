242f5986765e19f782acc8a458985cedeab8c7e2
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-05 21:00:42 +0300

    feat: init new route property for contacts screen

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/features/auth/screens/otp_screen.dart';
 import 'package:flutter_chat_app/features/auth/screens/user_information_screen.dart';
+import 'package:flutter_chat_app/features/select_contacts/screens/select_contacts_screen.dart';
 import './features/auth/screens/login_screen.dart';
 
 import './common/widgets/error.dart';
@@ -18,6 +19,9 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
     case UserInformationScreen.routeName:
       return MaterialPageRoute(
           builder: (context) => const UserInformationScreen());
+    case SelectContactsScreen.routeName:
+      return MaterialPageRoute(
+          builder: (context) => const SelectContactsScreen());
     default:
       return MaterialPageRoute(
         builder: (context) => const Scaffold(
