05d4945ba29178150a4078b0de90af001b035738
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-08 17:30:18 +0300

    feat: init new func to check selected contacts

--- lib/features/select_contacts/repository/select_contact_repository.dart
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -1,9 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screens/mobile_chat_screen.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '/common/utils/utils.dart';
+import '/models/user_model.dart';
 
-final selectContactRepositoryProvider =
+import '../constants/select_contacts_const.dart';
+
+final selectContactsRepositoryProvider =
     Provider(((ref) => SelectContactRepository(
           firebaseFirestore: FirebaseFirestore.instance,
         )));
@@ -23,5 +28,31 @@ class SelectContactRepository {
       debugPrint(e.toString());
     }
     return contacts;
+  }
+
+  void selectContact(Contact selectedContact, BuildContext context) async {
+    try {
+      var userCollection = await firebaseFirestore.collection("users").get();
+      bool isFound = false;
+
+      for (var document in userCollection.docs) {
+        var userData = UserModel.fromMap(document.data());
+        String selectedPhoneNumber = selectedContact.phones[0].number
+            .replaceAll(" ", "")
+            .replaceAll("-", "")
+            .replaceAll("(", "")
+            .replaceAll(")", "");
+        if (selectedPhoneNumber == userData.phoneNumber) {
+          isFound = true;
+          Navigator.pushNamed(context, MobileChatScreen.routeName);
+        }
+      }
+
+      if (!isFound) {
+        showSnackBar(context: context, content: selectContactSnackBarText);
+      }
+    } catch (e) {
+      showSnackBar(content: e.toString(), context: context);
+    }
   }
 }
