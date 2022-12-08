c0e6b390da64c059f946e627decaf5e49f8471a8
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-08 17:28:35 +0300

    feat: init new consts

--- lib/features/select_contacts/constants/select_contacts_const.dart
+++ lib/features/select_contacts/constants/select_contacts_const.dart
@@ -1,1 +1,7 @@
+import 'package:flutter/material.dart';
+
 const contactsAppBarText = "Select contact";
+
+const selectContactSnackBarText = "This number doesn't exist on this app.";
+
+const contactNameStyle = TextStyle(fontSize: 18);
