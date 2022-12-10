2dff054b19b11f2767b407335154eafefb1780ca
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-12-10 20:39:19 +0300

    feat: init method to get user data

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -114,4 +114,14 @@ class AuthRepository {
       showSnackBar(context: context, content: e.toString());
     }
   }
+
+  Stream<UserModel> userData(String userId) {
+    return firebaseFirestore
+        .collection("users")
+        .doc(userId)
+        .snapshots()
+        .map((event) => UserModel.fromMap(
+              event.data()!,
+            ));
+  }
 }
